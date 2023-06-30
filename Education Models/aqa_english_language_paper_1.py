a8b97bbd5566583ea62fb796a820b6e6bf3963c6
youseebaba <davidtiareh2@gmail.com>
2023-06-30 19:39:35 +0100

    small changes

--- Education Models/aqa_english_language_paper_1.py
+++ Education Models/aqa_english_language_paper_1.py
@@ -105,7 +105,106 @@ class Paper1 :
             question = self.question_maker(subsectionSource)
             return question, subsectionSource
     class Question3 : 
-        def descriptor(self, titleOfBook, bookType, pageNumber) : 
+        def descriptor(self, sourceExtract, titleOfBook, bookType, pageNumber, bookLength) :
+            describeExtractPrompt = f"""
+                                    , The book is {titleOfBook}, A {bookType}. This is an extract starting from page 
+                                    {pageNumber} out of {bookLength}
+
+                                    Create me a brief description of what this source is supposed to be. 
+                                    Keep the language as simple as the given examples, you should also ALWAYS start with 
+                                    'This text is from' then followed by where the extract is from. It should be very brief, 
+                                    to the point, and simply stated. Here are some examples of what you should output; note, 
+                                    this to help you with structuring your answer, but you don't need to say the exact words in the examples given. 
+                                    DO NOT mention the author name or the book title: 
+
+                                    (This text is from the opening of a novel. 
+                                    This text is from the middle of a short story.
+                                    This text is from the beginning of a novel. 
+                                    This text is from the end of a novel.
+                                    This text is from the middle of a novel .
+                                    This text is from the opening of a novelle.
+                                    This text is from the opening of a short story.) """
+            gptAgent = OpenAI() 
+            description = gptAgent.open_ai_gpt_call(sourceExtract, describeExtractPrompt)
+            return description
+        def final_model(self, description) : 
+            questionString = f""" You now need to think about the whole of the source.
+                                {description}
+                                How has the writer structured the text to interest you as a reader?
+                                You could write about:
+                                • what the writer focuses your attention on at the beginning of the source
+                                • how and why the writer changes this focus as the source develops
+                                • any other structural features that interest you.
+                                """ 
+            return questionString
+    class Question4 : 
+        def focus_question(self, extract) : 
+            question4PromptGPT4 = f"""
+                                . Now with this extract in mind, 
+                                Based on these three exam style questions, I want you to create a new once based on the extract I give you. 
+                                Here are the example questions: 
+                                ( Focus this part of your answer on the second part of the source, from line 25 to
+                                the end.
+                                A student said, ‘This part of the story, where Mr Fisher is marking homework,
+                                shows Tibbet’s story is better than Mr Fisher expected, and his reaction is
+                                extreme.’
+                                To what extent do you agree?
+                                In your response, you could:
+                                • consider your own impressions of what Mr Fisher expected Tibbet’s
+                                homework to be like
+                                • evaluate how the writer conveys Mr Fisher’s reaction to what he discovers
+                                • support your response with references to the text.
+
+                                Focus this part of your answer on the second part of the source, from line 24 to the
+                                end.
+                                A student said, ‘I wasn’t at all surprised by the disappearance of the stranger child
+                                at the end of the extract. The writer has left us in no doubt that she is just part of
+                                Rosie’s imagination.’
+                                To what extent do you agree?
+                                In your response, you could:
+                                • consider the disappearance of the stranger child
+                                • evaluate how the writer presents the stranger child
+                                • support your response with references to the text.
+
+                                Focus this part of your answer on the second part of the source, from line 20 to the
+                                end.
+                                A student said, ‘From the moment he arrives at Master’s compound, the writer
+                                portrays Ugwu’s feelings of pure excitement, but by the end it seems that he may
+                                be very disappointed.’
+                                To what extent do you agree?
+                                In your response, you could:
+                                • consider your own impressions of Ugwu’s feelings
+                                • evaluate how the write(r describes Ugwu’s feelings by the end
+                                • support your response with references to the text.) 
+
+                                , and here is the extract. Note, instead of saying 'from line (number)' just output (from line (STARTING SENTENCE HERE))', for that sentence, DO NOT add anything else, 
+                                just continue to the next section once the quote is made.
+                                
+                                """
+            gptAgent = OpenAI()
+            question4 = gptAgent.open_ai_gpt4_call(extract, question4PromptGPT4)
+            return question4
+    class Question5 : 
+        def introduction(self) :
+            introductionPrompt = "" #input prompt from google drive
+            gptAgent = OpenAI()
+            introduction = gptAgent.open_ai_gpt_call(prompt=introductionPrompt)
+            return introduction
+            #return introduction
+        def write_a_whatever(self) : 
+            writeAWhateverPrompt = "" #input prompt from google drive
+            gptAgent = OpenAI()
+            writeAWhatever = gptAgent.open_ai_gpt_call(prompt=writeAWhateverPrompt)
+            #return writeAWhatever
+        def describe(self) : 
+            describePrompt = ""#input prompt from google drive
+            gptAgent = OpenAI()
+            describe = gptAgent.open_ai_gpt_call(prompt=describePrompt)
+            #API call to dalle2, with describe as the input prompt.
+            #return describe, + describeImage
+
+        def final_model(self) : 
+            #Combine all three into one return for ease of use
 
 
             
